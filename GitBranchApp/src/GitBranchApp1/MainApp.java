8d85f5ddb08a58a2cce434bdcb1808cf3e6490fe
YuyaKung <yuya013131@gmail.com>
2023-01-07 14:11:45 +0800

    Add Math Class

--- GitBranchApp/src/GitBranchApp1/MainApp.java
+++ GitBranchApp/src/GitBranchApp1/MainApp.java
@@ -6,6 +6,16 @@ public class MainApp {
 		// TODO Auto-generated method stub
         //組長
 		System.out.println("1st Version");
+		//組長(新增的)
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
 	}
 
+}
+
+class Math{
+	int add(int a ,int b) {
+		return a+b;
+	}
 }
