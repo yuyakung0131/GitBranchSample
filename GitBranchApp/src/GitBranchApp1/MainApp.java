f47fcde4ef31f1f7b172456293b7e588ed7c52e6
YuyaKung <yuya013131@gmail.com>
2023-01-07 13:58:08 +0800

    1st Version

---
+++ GitBranchApp/src/GitBranchApp1/MainApp.java
@@ -0,0 +1,11 @@
+package GitBranchApp1;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //²Õªø
+		System.out.println("1st Version");
+	}
+
+}
